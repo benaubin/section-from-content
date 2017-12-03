dbf43d18086265b15d285225654e937708c87b80
Ben Aubin <ben@bensites.com>
2017-12-02 22:53:04 -0600

    Inline sections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var App = require('ghost-app');
-
 var marked = require('marked');
+var escapeRegExp = require('escape-string-regexp');
 
 marked.setOptions({
   renderer: new marked.Renderer(),
@@ -14,18 +14,33 @@ marked.setOptions({
 });
 
 var sectionFromContent = function(sectionID, content){
-  var section = null;
+  var section = null,
+      sectionHeader = "<!-- " + sectionID + ":",
+      sectionFooter = sectionID + "-->";
 
   for (line of content.split("\n")){
     if(section == null){
-      if(line.startsWith("<!-- " + sectionID + ":")) {
-        section = "";
+      if(line.startsWith(sectionHeader)) {
+        if(line.endsWith("-->")){
+          var key = escapeRegExp(sectionID)
+              exp = new RegExp("^<!--\\s*(?:" + key + "):\\s*(.+?)\\s*-->$"),
+            match = exp.exec(line);
+
+          return (match == null) ? "<script>console.log(JSON.parse('"+JSON.stringify({
+            line:line,
+            exp:exp,
+            match:match,
+            key:key,
+            sectionID: sectionID
+          })+"'))</script>" : match[1];
+        }
+        section = [];
       }
     } else {
-      if(line.startsWith(sectionID + "-->")){
-        return marked(section);
+      if(line.startsWith(sectionFooter)){
+        return marked(section.join("\n"));
       } else {
-        section += line + "\n";
+        section.push(line);
       }
     }
   }
